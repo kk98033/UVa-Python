613f8018983504d043ca64b1c8745f5757f1fd37
Chia Yu Yang <a8829037@gmail.com>
2023-03-22 06:25:31 +0800

    20230322

--- 10268.py
+++ 10268.py
@@ -8,22 +8,22 @@ from collections import defaultdict
 
 alphabet = {'a' : '1', 'b' : '2', 'c' : '3', 'd' : '4', 'e' : '5', 'f' : '6', 'g' : '7', 'h' : '8', 'i' : '9', 'j' : '10', 'k' : '11', 'l' : '12', 'm' : '13', 'n' : '14', 'o' : '15', 'p' : '16', 'q' : '17', 'r' : '18', 's' : '19', 't' : '20', 'u' : '21', 'v' : '22', 'w' : '23', 'x' : '24', 'y' : '25', 'z' : '26'}
 
-# UVa 10062 - Tell me the frequencies!
-def solve(s):
-    dic = {}
-    for i in s:
-        dic[i] = dic.get(i, 0) + 1
-    ans = sorted([[ord(key), val] for key, val in dic.items()], key=lambda x: (int(x[1]), -x[0]))
-    return '\n'.join([f'{key} {val}'for key, val in ans])
+# UVa 10268 - 498-bis
+def solve(x, nums):
+    if len(nums) < 2: return 0
+    ans = nums[-2]
+    # for i in range(len(nums)-3, -1, -1):
+    #     ans += nums[i] * (len(nums) - i - 1) * pow(x, (len(nums) - i - 1 - 1))
+    for i in range(len(nums)-2):
+        ans += nums[i] * (len(nums) - i - 1) * pow(x, (len(nums) - i - 1 - 1))
+    return ans
 
-count = 0
 while True:
     try:
-        s = input()
-        if count != 0: print()
-        if s:
-            print(solve(s))
-        count += 1
+        x = int(input())
+        nums = list(map(int, input().split()))
+
+        print(solve(x, nums))
     except EOFError:
         break
-# Accepted	PYTH3	0.040
+# Time limit exceeded	PYTH3	3.000 WTF DUDE???
