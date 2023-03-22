5d11337339e6661ca6e3f656622393b4d095dd6a
Chia Yu Yang <a8829037@gmail.com>
2023-03-22 10:02:22 +0800

    160

--- 160.py
+++ 160.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 alphabet = {'a' : '1', 'b' : '2', 'c' : '3', 'd' : '4', 'e' : '5', 'f' : '6', 'g' : '7', 'h' : '8', 'i' : '9', 'j' : '10', 'k' : '11', 'l' : '12', 'm' : '13', 'n' : '14', 'o' : '15', 'p' : '16', 'q' : '17', 'r' : '18', 's' : '19', 't' : '20', 'u' : '21', 'v' : '22', 'w' : '23', 'x' : '24', 'y' : '25', 'z' : '26'}
 
 # UVa 160 - Factors and Factorials
+# 2023/03/21/ CPE - 5
 def isPrime(n):
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
