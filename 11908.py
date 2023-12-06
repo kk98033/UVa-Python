73ac801aa0400992cfdeb80b380ade8fa42bfea0
Chia Yu Yang <a8829037@gmail.com>
2023-12-07 01:31:17 +0800

    update

--- 11908.py
+++ 11908.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 
 alphabet = {'a' : '1', 'b' : '2', 'c' : '3', 'd' : '4', 'e' : '5', 'f' : '6', 'g' : '7', 'h' : '8', 'i' : '9', 'j' : '10', 'k' : '11', 'l' : '12', 'm' : '13', 'n' : '14', 'o' : '15', 'p' : '16', 'q' : '17', 'r' : '18', 's' : '19', 't' : '20', 'u' : '21', 'v' : '22', 'w' : '23', 'x' : '24', 'y' : '25', 'z' : '26'}
 
+# UVa 11908 - Skyscraper
+import sys
 def solve(n, nums):
     # maxLen = max([i[0] + i[1] - 1 for i in nums])
     maxList = max(nums, key=lambda x: x[0] + x[1] - 1)
@@ -31,5 +33,7 @@ for t in range(T):
     n = int(input())
     nums = []
     for i in range(n):
-        nums.append(list(map(int, input().split())))
+        # nums.append(list(map(int, input().split())))
+        nums.append(list(map(int, sys.stdin.readline().split())))
     print(f'Case {t+1}: {solve(n, nums)}')
+# Accepted	PYTH3	1.440
