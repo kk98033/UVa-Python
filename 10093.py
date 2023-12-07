304e285ed489abbed2faeee1e3f63876010a808b
Chia Yu Yang <a8829037@gmail.com>
2023-12-07 23:43:32 +0800

    update

--- 10093.py
+++ 10093.py
@@ -10,36 +10,42 @@ alphabet = {'a' : '1', 'b' : '2', 'c' : '3', 'd' : '4', 'e' : '5', 'f' : '6', 'g
 
 # UVa 10093 - An Easy Problem!
 def generate():
+    # 建立一個字典來映射字元到對應的數值
     num = {}
     index = 0
+
+    # 將 '0' 到 '9' 映射到 0-9
     for i in range(ord('0'), ord('9')+1):
         num[chr(i)] = index
         index += 1
 
+    # 將 'A' 到 'Z' 映射到 10-35
     for i in range(ord('A'), ord('Z')+1):
         num[chr(i)] = index
         index += 1
 
+    # 將 'a' 到 'z' 映射到 36-61
     for i in range(ord('a'), ord('z')+1):
         num[chr(i)] = index
         index += 1
     return num
 
 def solve(s):
-    maxNum = max([num[i] for i in s if i in num])    
-    sumS = sum([num[i] for i in s if i in num])    
-    if maxNum == 0: return 2
+    maxNum = max([num[i] for i in s if i in num]) + 1 # 找出字串中最大的數值
+    sumS = sum([num[i] for i in s if i in num])  # 計算字串中所有字符對應數值的總和 
+    if maxNum <= 1: return 2
 
     while maxNum <= 62:
-        if sumS % maxNum == 0: 
-            return maxNum + 1
+        # 如果總和能被當前進制減一整除，則找到了答案
+        if sumS % (maxNum - 1) == 0: 
+            return maxNum
         maxNum += 1
     return 'such number is impossible!'
 
 num = generate()
 while True:
     try:
-        s = input().replace(' ', '')
+        s = input().replace(' ', '') # 讀取輸入並去除空格
         print(solve(s))
     except EOFError:
         break
