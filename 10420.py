d5150b617235f51cfe57f471230ae8cedff5b549
Chia Yu Yang <a8829037@gmail.com>
2024-04-07 20:04:28 +0800

    10420

--- 10420.py
+++ 10420.py
@@ -15,6 +15,6 @@ for t in range(T):
     country = input().split()[0]
     count[country] = count.get(country, 0) + 1
 
-for country in sorted(list(count)):
-    print(f'{country} {count[country]}')
+for country, count in sorted(count.items()):
+    print(f'{country} {count}')
 # Accepted	PYTH3	0.070
