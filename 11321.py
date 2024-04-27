c1403bfef3d96ee87ae3205dd0443cf9b2141386
Chia Yu Yang <a8829037@gmail.com>
2024-04-27 18:40:21 +0800

    update

--- 11321.py
+++ 11321.py
@@ -11,42 +11,27 @@ alphabet = {'a' : '1', 'b' : '2', 'c' : '3', 'd' : '4', 'e' : '5', 'f' : '6', 'g
 
 
 # UVa 11321 - Sort! Sort!! and Sort!!!
-# https://stackoverflow.com/questions/5213033/sort-a-list-of-lists-with-a-custom-compare-function
-from functools import cmp_to_key
 def solve(n, m, nums):
-    # 1. 先利用每個數字除以M的餘數由小到大排
-    # 2. 若排序中比較的兩數為一奇一偶且兩數除以M的餘數相等，則奇數要排在偶數前面。
-    # 3. 若兩奇數除以M餘數大小相等，則原本數值較大的奇數排在前面。
-    # 4. 若兩偶數除以M餘數大小相等，則較小的偶數排在前面。
-    def comapre(item1, item2):
-        # return a negative value (< 0) when the left item should be sorted before the right item
-        # return a positive value (> 0) when the left item should be sorted after the right item
+    '''
+        1. 數字按照它們除以 M 的餘數升序排列。
+        2. 如果兩個數的餘數相同，奇數要排在偶數前面。
+        3. 如果兩個數的餘數相同且都是奇數，則數值較大的排在前面。
+        4. 如果兩個數的餘數相同且都是偶數，則數值較小的排在前面。
+    '''
+    # https://stackoverflow.com/a/46969300
+    # def mod(a, b):  
+    #     return abs(a)%abs(b)*(1,-1)[a<0]
+    def mod(a, b):
+        result = abs(a) % abs(b)  # 先計算絕對值的模
+        if a < 0:
+            result *= -1  # 如果 a 是負數，使結果為負
+        return result
+    def sort_key(x):
+        return (mod(x, m), x % 2 == 0, -x if x % 2 != 0 else x)
 
-        modulo1, modulo2 = abs(item1) % m, abs(item2) % m
-        if item1 < 0 and m > 0: modulo1 = -modulo1
-        if item2 < 0 and m > 0: modulo2 = -modulo2
-
-        if modulo1 != modulo2: # rule 1
-            if modulo1 > modulo2:
-                return 1
-            else:
-                return -1
-        
-        else: # equal
-            if item1 % 2 != item2 % 2: # rule 2
-                if item1 % 2 == 1: # odd
-                    return -1
-                else: # even
-                    return 1
-            
-            if item1 % 2 == 1 and item2 % 2 == 1: # odd
-                return item2 - item1
-            
-            if item1 % 2 == 0 and item2 % 2 == 0: # even
-                return item1 - item2
-
-    ans = sorted(nums, key=cmp_to_key(comapre))
-    return '\n'.join(map(str, ans))
+    ans = sorted(nums, key=sort_key)
+    # ans = sorted(nums, key=lambda x: (mod(x, m), x % 2 == 0, -x if x % 2 != 0 else x)) # 用 lambda 也可以
+    return '\n'.join(map(str, ans)) 
 
 while True:
     n, m = list(map(int, input().split()))
@@ -57,4 +42,4 @@ while True:
         nums.append(int(input()))
 
     print(solve(n, m, nums))
-# Accepted	PYTH3	2.610
+# Accepted	PYTH3	1.160
